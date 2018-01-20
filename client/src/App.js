cf2259486b12b2525831405978020d4a4441546a
Jordan Porter <jporter@ngacres.com>
2018-01-20 12:45:27 -0700

    view inv

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import ViewInventory from './Components/ViewInventory/ViewInventory';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
+        <ViewInventory/>
       </div>
     );
   }
