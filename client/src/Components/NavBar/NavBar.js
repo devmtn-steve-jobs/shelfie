24b4dd3754a085fecb7bb023a7a3c264ffaece91
Aaron Martinez <aaronmartinez6@hotmail.com>
2018-01-20 12:06:52 -0700

    Add custom font, Home NavBar not quite but almost

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,14 +1,31 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import './NavBar.css';
 import logo from './logo.png';
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <div className="Background">
-                <img src={logo} alt=""/>
-            </div>
-        )
-    }
+    // constructor() {
+    //     super()
+    //     this.state = {
 
+    //     }
+    // }
+    render() {
+        {
+            if (this.props.currentShelf) {
+                return (
+                    <div className="Background">
+                        <img src={logo} alt="" />
+                        <h1 className='title' >SHELFIE</h1>
+                    </div>
+                )
+            } else if (this.props.currentShelf && this.props.currentBin) {
+                return (
+                    <div className="HomeBackground">
+                        <img src={logo} alt="" className="HomeImage" />
+                        <h1 className='HomeTitle' >SHELFIE</h1>
+                    </div>
+                )
+            }
+        }
+    }
 }
