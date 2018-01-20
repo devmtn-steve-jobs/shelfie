6666b99cd7c55de6da2b7765b3b0639e240e4686
Aaron Martinez <aaronmartinez6@hotmail.com>
2018-01-20 10:44:27 -0700

    Get NavBar to render, Set up routes

---
+++ client/src/Components/ShelfList/ShelfList.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class ShelfList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
