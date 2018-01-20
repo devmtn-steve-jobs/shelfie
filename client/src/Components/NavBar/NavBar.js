9417438f9ae8f1bab1d331baceea7e8da49d15c0
Aaron Martinez <aaronmartinez6@hotmail.com>
2018-01-20 13:04:11 -0700

    Ran out of time, Progress in the NavBar

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -11,18 +11,34 @@ export default class NavBar extends Component {
     // }
     render() {
         {
-            if (this.props.currentShelf) {
-                return (
-                    <div className="Background">
-                        <img src={logo} alt="" />
-                        <h1 className='title' >SHELFIE</h1>
-                    </div>
-                )
-            } else if (this.props.currentShelf && this.props.currentBin) {
+            // Shelf and Bin Selected
+            if (this.props.currentShelf && this.props.currentBin) {
                 return (
                     <div className="HomeBackground">
                         <img src={logo} alt="" className="HomeImage" />
-                        <h1 className='HomeTitle' >SHELFIE</h1>
+                        <h1 className='Title' >SHELFIE</h1>
+                    </div>
+                )
+                // Shelf selected
+            } else if (this.props.currentShelf) {
+                return (
+                    <div className="HomeBackground">
+                        <div>
+                            <img src={logo} alt="" className="IconHomeButton" />
+                            {/* <h1 className='Title' >{this.props.currentShelf}</h1> */}
+
+                        </div>
+                        <div className="ShelfNavTitle">
+                            {this.props.currentShelf}
+                        </div>
+                    </div>
+                )
+                // No shelf selected
+            } else {
+                return (
+                    <div className="HomeBackground">
+                        <img src={logo} alt="" className="HomeImage" />
+                        <h1 className='Title' >SHELFIE</h1>
                     </div>
                 )
             }
