51bbff85803a3734299c19e857ba3d01b04a6897
Jaylan Jones <jaylanjones421@gmail.com>
2018-01-20 12:19:43 -0600

    Built ShelfList and changed props to this.props in ShelfLink

---
+++ client/src/Components/ShelfList/ShelfList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ShelfLink from '../ShelfLink/ShelfLink';
+
+class ShelfList extends Component {
+    render() {
+        const shelves = ['A','B','C','D'];
+        const shelving=shelves.map((x,i)=>{
+            return (
+                <ShelfLink key={i} shelfID={x}/>
+            )
+        })
+
+        return (
+            <div className='shelvesContainer'>
+                { shelving }
+            </div>
+        );
+    }
+}
+
+export default ShelfList;
